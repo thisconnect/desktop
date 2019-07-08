d674e8c40bcff3c031369462942fabd1788e830b
thisconnect <hi@this-connect.me>
2019-07-08 08:11:01 +0200

    adds fetching data.json example

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: null
+    }
+  }
+  componentDidMount() {
+    fetch('./data.json')
+      .then(data => data.json())
+      .then(data => this.setState({ data }))
+      .catch(console.error)
+  }
   render() {
     return (
       <div className="App">
@@ -15,6 +27,9 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {this.state.data && `fetched ${this.state.data.length} items from data.json`}
         </p>
       </div>
     )
