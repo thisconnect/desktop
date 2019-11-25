617b2ed34e0563cf8de1a919eb264eb3ac8a7dc5
thisconnect <hi@this-connect.me>
2019-11-25 14:01:36 +0100

    open external links in default browser

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron')
-const dev = require('electron-is-dev')
+const { app, BrowserWindow, shell } = require('electron')
+const DEV = require('electron-is-dev')
+const URL = require('url').URL
 
 let win = null
 
@@ -17,6 +18,42 @@ app.on('activate', () => {
   }
 })
 
+
+const open = async (e, url) => {
+  e.preventDefault()
+  const { href } = new URL(url)
+  await shell.openExternal(href)
+}
+
+app.on('web-contents-created', (event, contents) => {
+  contents.on('new-window', open)
+  contents.on('will-navigate', (e, url) => {
+    const { href } = new URL(url)
+    if (DEV) {
+      if (
+        !href.startsWith('http://localhost:3000/')
+        && !href.startsWith('devtools:')
+      ) {
+        open(e, href)
+      }
+    }
+    if (!DEV) {
+      if (
+        (!href.startsWith('file://'))
+        || (!href.includes('/build/'))
+      ) {
+        try {
+          open(e, href)
+
+        } catch (e) {
+          console.log('counldnt open', href)
+          console.log(e)
+        }
+      }
+    }
+  })
+})
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
@@ -25,15 +62,23 @@ app.on('window-all-closed', () => {
 
 function openWindow(){
   return new Promise((resolve, reject) => {
-    resolve(new BrowserWindow({ width: 800, height: 600 }))
+    resolve(new BrowserWindow({
+      width: 800,
+      height: 600,
+      // webPreferences: {
+      //   contextIsolation: true,
+      //   preload: path.join(app.getAppPath(), 'preload.js'),
+      //   webSecurity: false
+      // }
+    }))
   })
     .then(main => {
-      const loc = dev ? 'http://localhost:3000' : `file://${__dirname}/build/index.html`
+      const loc = DEV ? 'http://localhost:3000' : `file://${__dirname}/build/index.html`
       main.loadURL(loc)
       main.on('closed', () => {
         win = null
       })
-      if (dev){
+      if (DEV){
         main.webContents.openDevTools()
       }
       return main
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, StrictMode } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
@@ -17,24 +17,37 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>
-            Welcome to React<br />
-            <small>{process.env.NODE_ENV}</small>
-          </h2>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {this.state.data ? `fetched ${this.state.data.length} items from data.json` : 'no data.json'}
+      <StrictMode>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>
+              Welcome to <a href="?">React</a> at {process.env.PUBLIC_URL}<br />
+              <small>{process.env.NODE_ENV}</small>
+            </h2>
+            {
+              // eslint-disable-next-line no-restricted-globals
+              location.href
+            }<br />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a><br />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React (target="_blank")
+            </a>
+            {this.state.data ? `fetched ${this.state.data.length} items from data.json` : 'no data.json'}
+          </div>
         </div>
-      </div>
+      </StrictMode>
     )
   }
 }
